ecdabd7b6ab8cd65596f0177ba4b1fb77a7c9a7b
Tsai Li Ming <tsai.liming@gmail.com>
2021-03-25 22:43:05 +0800

    Update spool dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
+if [[ ! -d /var/spool/squid/00 ]]; then
     echo "Initializing cache..."
     squid -f /etc/squid/squid.conf --foreground -d 1 -z
 fi
